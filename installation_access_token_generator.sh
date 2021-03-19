10bbf53f40f686b792caf60fd0b17cb96e1a3f37
cvonlehe <cvonlehe@squarespace.com>
2021-03-19 15:54:01 -0400

    switching to js

--- installation_access_token_generator.sh
+++ installation_access_token_generator.sh
@@ -4,10 +4,20 @@ INSTALLATION_ACCESS_TOKEN=$(curl -X POST \
 -H "Authorization: Bearer $1" \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/app/installations/15503724/access_tokens | ./node_modules/node-jq/bin/jq -r '.token')
+echo INSTALLATION_ACCESS_TOKEN
+# curl -i \
+# -H "Authorization: token $INSTALLATION_ACCESS_TOKEN" \
+# -H "Accept: application/vnd.github.v3+json" \
+# https://api.github.com/installation/repositories
 
-curl \
-  -X POST \
-  -H "Authorization: token $INSTALLATION_ACCESS_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/cvlehe/fem-fox-game/pulls \
-  -d '{"head":"test/automated-pr","base":"test/automated-pr-master", "title":"Hello PR"}'
+# # curl -i \
+# # -H "Authorization: token $INSTALLATION_ACCESS_TOKEN" \
+# # -H "Accept: application/vnd.github.v3+json" \
+# # https://api.github.com/repos/cvlehe/fem-fox-game/branches
+
+# curl \
+#   -X POST \
+#   -H "Authorization: token $INSTALLATION_ACCESS_TOKEN" \
+#   -H "Accept: application/vnd.github.v3+json" \
+#   https://api.github.com/repos/cvlehe/fem-fox-game/pulls \
+#   -d '{"head":"test/automated-pr","base":"test/automated-pr-master", "title":"Hello PR"}'
--- jwt_generator.rb
+++ jwt_generator.rb
@@ -8,19 +8,19 @@ pem = Dir.glob("./*.private-key.pem").first()
 # Private key contents
 private_pem = File.read(pem)
 private_key = OpenSSL::PKey::RSA.new(private_pem)
+puts private_key
+# # Generate the JWT
+# payload = {
+#   # issued at time, 60 seconds in the past to allow for clock drift
+#   iat: Time.now.to_i - 60,
+#   # JWT expiration time (10 minute maximum)
+#   exp: Time.now.to_i + (10 * 60),
+#   # GitHub App's identifier
+#   iss: 105926
+# }
 
-# Generate the JWT
-payload = {
-  # issued at time, 60 seconds in the past to allow for clock drift
-  iat: Time.now.to_i - 60,
-  # JWT expiration time (10 minute maximum)
-  exp: Time.now.to_i + (10 * 60),
-  # GitHub App's identifier
-  iss: 105926
-}
+# jwt = JWT.encode(payload, private_key, "RS256")
 
-jwt = JWT.encode(payload, private_key, "RS256")
+# # internetAccessTokenRes = exec("./installation_access_token_generator.sh", jwt)
 
-internetAccessTokenRes = exec("./installation_access_token_generator.sh", jwt)
-
-puts internetAccessTokenRes
+# puts jwt

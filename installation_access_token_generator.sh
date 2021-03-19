cd5c697bce1eb8fe7e76ad756ab4a8a746478a72
cvonlehe <cvonlehe@squarespace.com>
2021-03-19 14:48:27 -0400

    added dynamic PEM file

--- installation_access_token_generator.sh
+++ installation_access_token_generator.sh
@@ -5,16 +5,6 @@ INSTALLATION_ACCESS_TOKEN=$(curl -X POST \
 -H "Accept: application/vnd.github.v3+json" \
 https://api.github.com/app/installations/15503724/access_tokens | ./node_modules/node-jq/bin/jq -r '.token')
 
-# curl -i \
-# -H "Authorization: token $INSTALLATION_ACCESS_TOKEN" \
-# -H "Accept: application/vnd.github.v3+json" \
-# https://api.github.com/installation/repositories
-
-# # curl -i \
-# # -H "Authorization: token $INSTALLATION_ACCESS_TOKEN" \
-# # -H "Accept: application/vnd.github.v3+json" \
-# # https://api.github.com/repos/cvlehe/fem-fox-game/branches
-
 curl \
   -X POST \
   -H "Authorization: token $INSTALLATION_ACCESS_TOKEN" \
